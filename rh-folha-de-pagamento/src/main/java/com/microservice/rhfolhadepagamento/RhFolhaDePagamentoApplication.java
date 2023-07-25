51835b8470123d7332fe767276308ac118a1a857
Willardy <willardy.oliveira@solutis.com.br>
2023-07-24 23:29:12 -0300

    Criado Microserviço de Folha de Pagamento

---
+++ rh-folha-de-pagamento/src/main/java/com/microservice/rhfolhadepagamento/RhFolhaDePagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.rhfolhadepagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhFolhaDePagamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhFolhaDePagamentoApplication.class, args);
+    }
+
+}
---
+++ rh-folha-de-pagamento/src/test/java/com/microservice/rhfolhadepagamento/RhFolhaDePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.rhfolhadepagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhFolhaDePagamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
