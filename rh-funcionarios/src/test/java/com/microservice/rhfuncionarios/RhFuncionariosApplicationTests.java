b2de0639ab38c03eaf07973f7332655243bc2ddc
Willardy <willardy.oliveira@solutis.com.br>
2023-07-24 22:19:26 -0300

    Microservico de Funcionarios

---
+++ rh-funcionarios/src/test/java/com/microservice/rhfuncionarios/RhFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.rhfuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhFuncionariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
