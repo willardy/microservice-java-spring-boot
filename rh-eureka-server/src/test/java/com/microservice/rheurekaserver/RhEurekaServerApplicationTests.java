fb4a5713e4cc45baf6075925161cb62a46889fad
Willardy <willardy.oliveira@solutis.com.br>
2023-07-31 12:04:35 -0300

    Criado Microserviço do Eureka Server

---
+++ rh-eureka-server/src/test/java/com/microservice/rheurekaserver/RhEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.rheurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
